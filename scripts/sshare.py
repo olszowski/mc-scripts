c23e50759b948d22d49588f5e210b4a51a661022
Daniel Olszowski <daniel.olszowski@uk.tesco.com>
2018-09-11 22:07:18 +0200

    terminal fix

--- scripts/sshare.py
+++ scripts/sshare.py
@@ -1,5 +1,5 @@
 def get_share_info(user):
-    command = 'sshare -U {user} -l -m -P'
+    command = 'sshare -U {user_id} -l -m -P'
     from terminal import run_command
-    output = run_command(command, user=user)
+    output = run_command(command, user_id=user)
     print(output)
