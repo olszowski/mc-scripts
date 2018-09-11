63265829db00553e8b4298a21ece96400292fe9c
Daniel Olszowski <daniel.olszowski@uk.tesco.com>
2018-09-11 22:40:07 +0200

    sshare impl

--- scripts/sshare.py
+++ scripts/sshare.py
@@ -1,5 +1,11 @@
-def get_share_info(user):
+def get_sshare_output(user):
     command = 'sshare -U {user_id} -l -m -P'
     from terminal import run_command
-    output = run_command(command, user_id=user)
-    print(output)
+    return run_command(command, user_id=user)
+
+
+def get_user_fairshare(sshare_output):
+    lines = sshare_output.split('\n')
+    import csv
+    reader = csv.DictReader(lines, delimiter="|")
+    return float(next(reader)['FairShare'])
